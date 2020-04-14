b48ef079e4d95e5d505771498f6c1056a71f650c
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-14 20:57:32 +0530

    TextAdded_by_fs

--- Node/Nodemodule/fs_demo.js
+++ Node/Nodemodule/fs_demo.js
@@ -2,10 +2,17 @@ const fs=require('fs');
 const path =require('path');
 
 //Create folder
- fs.mkdir(path.join(__dirname,'/test1'),{},function(err){
-     if(err) throw err;
-     else{
-         console.log("folder created");
-     }
- })
+//  fs.mkdir(path.join(__dirname,'/test1'),{},function(err){
+//      if(err) throw err;
+//      else{
+//          console.log("folder created");
+//      }
+//  })
   //{}-->blank object,function(err)--->call back function
+
+
+//create and write to file
+fs.writeFile(path.join(__dirname,'/test1','hello.txt'),'kuch bhi!!!',function(err){
+    if(err) throw err;
+    console.log("file created")
+})
