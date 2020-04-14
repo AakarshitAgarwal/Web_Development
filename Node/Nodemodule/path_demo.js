193aecfd03f8d35346516a0ac0794d676008e110
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-14 19:55:25 +0530

    path_fn_complete

--- Node/Nodemodule/path_demo.js
+++ Node/Nodemodule/path_demo.js
@@ -4,4 +4,13 @@ const path =require('path');
 // console.log(path);
 
 //Base File name
-console.log(path.basename(__filename));
+// console.log(path.basename(__filename));
+
+//Extension Name
+// console.log(path.extname(__filename));
+
+//Object Form
+// console.log(path.parse(__filename).name);
+
+//make a url of file
+// console.log(path.join(__dirname,'public','index.html'));
