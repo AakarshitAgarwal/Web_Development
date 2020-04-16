b190e054bce4abad8b04f4c699e8325f9115a94d
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-16 20:49:34 +0530

    adding_members

---
+++ Express/RESTAPI/Members.js
@@ -0,0 +1,18 @@
+const members=[{
+    id:1,
+    name:"Aakarshit",
+    email:"geek@gmail.com",
+    status:"inactive"
+},{
+    id:2,
+    name:"Atishay",
+    email:"CR@gmail.com",
+    status:"inactive"
+},{
+    id:3,
+    name:"Anurag",
+    email:"chill@gmail.com",
+    status:"active"
+}]
+
+module.exports=members
--- Express/RESTAPI/index.js
+++ Express/RESTAPI/index.js
@@ -1,6 +1,12 @@
 const express =require('express');
-
+const members =require('./Members');
 const app=express();
 
+app.get('/api/members',(req,res)=>{
+    res.json(members)
+})
+app.get('/api/members/:id',(req,res)=>{
+    console.log(req.params.id);   //params-->parameter
+})
 const PORT=3000;
 app.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
