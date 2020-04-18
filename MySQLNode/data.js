23b33b60dbd12a1e9e5f7b061fd4ab2982cff538
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-19 01:50:16 +0530

    NodeSQL

--- MySQLNode/data.js
+++ MySQLNode/data.js
@@ -1,1 +1,14 @@
 //I will store some data afterwards
+const mysql=require('mysql');
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'testing'
+})
+db.connect((err)=>{
+    if(err) throw err;
+    console.log('Database Connected');
+})
+
+module.exports=db;
--- MySQLNode/index.js
+++ MySQLNode/index.js
@@ -1,14 +1,18 @@
-// const express=require('express');
-// const mysql =require('mysql');
-// //const db=require('./data');
-// const app=express();
-// app.use(express.json());  //middleware
-// // app.get('/insertpests',(req,res)=>{
-// //     let post={title:req.body.title,body:req.body.body,email:req.body.email};
-// //     let sql ="INSERT INTO 'posts' SET ?",  //PLACEHOLDER
-// //     db.query(sql,post,(err,result)=>{
-// //         if(err) throw err;
-// //         console.log("Data Saved");
-// //         res.send(result);
-// //     })
-// // })
+const express=require('express');
+const mysql =require('mysql');
+const db=require('./data');
+const app=express();
+app.use(express.json());  //middleware
+app.get('/insertposts',(req,res)=>{
+    //let post ={title:'Post One',body:'This is Post One',email:'aakarshit@gmail.com'};
+    let post={title:req.body.title,body:req.body.body,email:req.body.email};
+    let sql ="INSERT INTO `posts` SET ?";  //PLACEHOLDER
+    db.query(sql,post,(err,result)=>{
+        if(err) throw err;
+        console.log("Data Saved");
+        res.send(result);
+    })
+})
+const PORT=5000;
+app.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
+
