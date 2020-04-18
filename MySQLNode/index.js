b4d4deb90c15557de7c59051fd8920420ed88178
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-19 01:26:37 +0530

    NodeSQL

---
+++ MySQLNode/index.js
@@ -0,0 +1,14 @@
+// const express=require('express');
+// const mysql =require('mysql');
+// //const db=require('./data');
+// const app=express();
+// app.use(express.json());  //middleware
+// // app.get('/insertpests',(req,res)=>{
+// //     let post={title:req.body.title,body:req.body.body,email:req.body.email};
+// //     let sql ="INSERT INTO 'posts' SET ?",  //PLACEHOLDER
+// //     db.query(sql,post,(err,result)=>{
+// //         if(err) throw err;
+// //         console.log("Data Saved");
+// //         res.send(result);
+// //     })
+// // })
