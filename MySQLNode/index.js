8f32e8bdd1f3a9b6bb81eb5170ac102f2820ebca
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-21 03:18:36 +0530

    CRUD_SQLnode

--- MySQLNode/index.js
+++ MySQLNode/index.js
@@ -4,7 +4,7 @@ const db=require('./data');
 const app=express();
 app.use(express.json());  //middleware
 app.get('/insertposts',(req,res)=>{
-    //let post ={title:'Post One',body:'This is Post One',email:'aakarshit@gmail.com'};
+    // let post ={title:'Post One',body:'This is Post One',email:'aakarshit@gmail.com'};
     let post={title:req.body.title,body:req.body.body,email:req.body.email};
     let sql ="INSERT INTO `posts` SET ?";  //PLACEHOLDER
     db.query(sql,post,(err,result)=>{
@@ -13,6 +13,49 @@ app.get('/insertposts',(req,res)=>{
         res.send(result);
     })
 })
+
+//fetch the data
+app.get('/showalldata',(req,res)=>{
+    let sql ="SELECT * FROM `posts`";
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//fetch particular data
+app.get('/showalldata/:uid',(req,res)=>{
+    let sql =`SELECT * FROM posts where id=${req.params.uid}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//Update the data
+app.get('/updatepost/:uid',(req,res)=>{
+    let newTitle ="Post Four"
+    let sql =`UPDATE posts SET title='${newTitle}' where id=${req.params.uid}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//Delete item
+app.get('/delete/:id',(req,res)=>{
+    let sql=`DELETE FROM posts where id=${req.params.id}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })    
+})
+
+
 const PORT=5000;
 app.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
 
