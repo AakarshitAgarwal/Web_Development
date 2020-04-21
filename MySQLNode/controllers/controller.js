cab2a7f309a8ebcb43b9442bd0a7607eb6cc8e8d
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-04-21 14:55:20 +0530

    Business_logic

---
+++ MySQLNode/controllers/controller.js
@@ -0,0 +1,62 @@
+const express=require('express');
+const mysql =require('mysql');
+const db=require('../data');
+
+const router =express.Router(); //use to join two URL
+
+
+
+//insert data
+router.get('/insertposts',(req,res)=>{  //while localhost:5000/insertposts
+    // let post ={title:'Post One',body:'This is Post One',email:'aakarshit@gmail.com'};
+    let post={title:req.body.title,body:req.body.body,email:req.body.email};
+    let sql ="INSERT INTO `posts` SET ?";  //PLACEHOLDER
+    db.query(sql,post,(err,result)=>{
+        if(err) throw err;
+        console.log("Data Saved");
+        res.send(result);
+    })
+})
+
+//fetch the data
+router.get('/showalldata',(req,res)=>{
+    let sql ="SELECT * FROM `posts`";
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//fetch particular data
+router.get('/showalldata/:uid',(req,res)=>{
+    let sql =`SELECT * FROM posts where id=${req.params.uid}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//Update the data
+router.get('/updatepost/:uid',(req,res)=>{
+    let newTitle ="Post Four"
+    let sql =`UPDATE posts SET title='${newTitle}' where id=${req.params.uid}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+//Delete item
+router.get('/delete/:id',(req,res)=>{
+    let sql=`DELETE FROM posts where id=${req.params.id}`;
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })    
+})
+
+module.exports=router;
--- MySQLNode/index.js
+++ MySQLNode/index.js
@@ -2,59 +2,9 @@ const express=require('express');
 const mysql =require('mysql');
 const db=require('./data');
 const app=express();
-app.use(express.json());  //middleware
-app.get('/insertposts',(req,res)=>{
-    // let post ={title:'Post One',body:'This is Post One',email:'aakarshit@gmail.com'};
-    let post={title:req.body.title,body:req.body.body,email:req.body.email};
-    let sql ="INSERT INTO `posts` SET ?";  //PLACEHOLDER
-    db.query(sql,post,(err,result)=>{
-        if(err) throw err;
-        console.log("Data Saved");
-        res.send(result);
-    })
-})
+app.use(express.json());  //middleware-->executes before the remaining code run(i.e at localhost:5000/ port)
 
-//fetch the data
-app.get('/showalldata',(req,res)=>{
-    let sql ="SELECT * FROM `posts`";
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    })
-})
-
-//fetch particular data
-app.get('/showalldata/:uid',(req,res)=>{
-    let sql =`SELECT * FROM posts where id=${req.params.uid}`;
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    })
-})
-
-//Update the data
-app.get('/updatepost/:uid',(req,res)=>{
-    let newTitle ="Post Four"
-    let sql =`UPDATE posts SET title='${newTitle}' where id=${req.params.uid}`;
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    })
-})
-
-//Delete item
-app.get('/delete/:id',(req,res)=>{
-    let sql=`DELETE FROM posts where id=${req.params.id}`;
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    })    
-})
-
+app.use('/',require('./controllers/controller'));
 
 const PORT=5000;
 app.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
